b4346289585bae21d4a12bc44e3c93d52d8354ae
BusaBusa <61671091+Busaa@users.noreply.github.com>
2024-02-12 02:23:57 -0300

    Update menu.h

--- Trabalho-UNO-ArthurBusanello-BrunoFazzioni/TADs/menu.h
+++ Trabalho-UNO-ArthurBusanello-BrunoFazzioni/TADs/menu.h
@@ -6,10 +6,12 @@
 
 #include "raylib.h"
 
+//Structures
 
 typedef enum MenuOption { //Defines the possible options the menu 
     PLAY,
     CONFIGURATION,
+    LOAD,
     LEADERBOARD,
     EXIT
 }MenuOption;
@@ -18,9 +20,20 @@ typedef struct Menu { //Define the structure menu, that will always have one of 
     MenuOption currentOption;
 }Menu;
 
+typedef struct Audio{ //For Music, background ost etc
+    bool muted;
+    float volume;
+    Sound sound;
+}Audio;
+
+//Functions
+
 Menu* InitMenu(void); //function to Inicializate the menu 
 void UpdateMenu(Menu* menu); //Updates the Menu
 void DrawMenu(Menu* menu);
 void ProcessMenuInput(Menu* menu);
+void PlayAudio(Audio* audio);
+Audio* InitAudio(const char* filename);
+void UnloadAudio(Audio* audio);
 
 #endif // MENU_H end
