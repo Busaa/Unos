9b16e1a7a29338b71da99405c6e19af4dd044803
Bruno Fazzioni <79119293+BigMussila@users.noreply.github.com>
2024-02-11 23:56:46 -0300

    Update Bruno2

--- unos_src/main.c
+++ unos_src/main.c
@@ -3,14 +3,26 @@
 #include "raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //TADS
 #include "menu.c"
+#include "Uno.h"
+#include "pilha_enc.h"
+#include "fila_enc.h"
+#include "lista_enc_dupla.h"
+#include "lista_circ_enc_dupla.h"
+#include "deque_enc.h"
 
 
 //Main
 int main(void)
 {
+    // Iniciando variáveis
+    PilhaEnc baralho;
+    PilhaEnc mesa;
+    ListaCirc2 ordem_jogadores;
+    
     
     //Screen Initialization
     const int screenWidth = 800;
